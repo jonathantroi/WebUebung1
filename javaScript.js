f25354d5e13e43376a687cf88b2199658c89e378
Jonathan Troi <troi.jonathan@gmail.com>
2017-09-19 14:29:24 +0200

    updated

--- javaScript.js
+++ javaScript.js
@@ -52,6 +52,7 @@ function change(colorChanger){
 var param = false;
 var par1 = "";
 var par2 = "";
+var calculation = "";
 
 function calc(){
 	var operation = document.getElementById("op").value;
@@ -61,16 +62,16 @@ function calc(){
 
 	switch(operation){
 		case "+":
-			document.getElementById("calcOutput3").innerHTML = first + second;
+			document.getElementById("calcOutput").innerHTML = first + second;
 		break;
 		case "-":
-			document.getElementById("calcOutput3").innerHTML = first - second;
+			document.getElementById("calcOutput").innerHTML = first - second;
 		break;
 		case "*":
-			document.getElementById("calcOutput3").innerHTML = first * second;
+			document.getElementById("calcOutput").innerHTML = first * second;
 		break;
 		case "/":
-			document.getElementById("calcOutput3").innerHTML = first / second;
+			document.getElementById("calcOutput").innerHTML = first / second;
 		break;
 		default:
 		break;
@@ -78,15 +79,20 @@ function calc(){
 
 	par1 = "";
 	par2 = "";
+
+	document.getElementById("calcOutput").innerHTML = Math.floor(eval(calculation));
+	calculation = "";
 }
 
 function addNumber(num){
+	calculation += num;
 	if(document.getElementById("opBox").checked == false){
 		par1 += num;
-		document.getElementById("calcOutput").innerHTML = par1;					
+		document.getElementById("calcOutput").innerHTML = par1;
+
 	}else{
 		par2 += num;
-		document.getElementById("calcOutput2").innerHTML = par1;
+		document.getElementById("calcOutput").innerHTML = par1;
 	}
 
 }
@@ -94,7 +100,7 @@ function addNumber(num){
 function setTrue(status){
 	calcCheck = true;
 }
-
+//Math.floor(eval(zahl))
 function evaluateCalc(){
 	var first = parseInt(document.getElementById("firstNum").value);
 	var second = parseInt(document.getElementById("secondNum").value);
@@ -120,7 +126,7 @@ function evaluateCalc(){
 
 var slideIndex = 1;
 
-showDivs(slideIndex);
+//showDivs(slideIndex);
 
 function plusDivs(n){
 	showDivs(slideIndex += n);
@@ -128,7 +134,7 @@ function plusDivs(n){
 
 function showDivs(n){
 	var i;
-	var x = document.getElementByClassName("mySlides");
+	var x = document.getElementsByClassName("mySlides");
 
 	if(n > x.length){
 		slideIndex = 1;
@@ -143,4 +149,63 @@ function showDivs(n){
 	}
 
 	x[slideIndex-1].style.display = "block";
+}
+
+function getAllParagraphs(){
+	var x = document.getElementsByTagName("p");
+
+	for(var i = 0; i < x.length; i++){
+		x[i].style.backgroundColor = "yellow";
+	}
+}
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
+
+function getBrowserVersionAndLanguage(){
+	document.getElementById("browser").innerHTML = "BrowserVersion: " + navigator.appCodeName +
+	"            BrowserLanguage: " + navigator.language;
+}
+
+function loadAjaxData(){
+	var xhttp = new XMLHttpRequest();
+  	xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     	document.getElementById("ajaxData").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajaxData.txt", true);
+  xhttp.send();
 }
